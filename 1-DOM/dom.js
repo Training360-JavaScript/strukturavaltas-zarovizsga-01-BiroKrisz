1d4cca3df1543029d194d162a1feb55688cff1ef
BiroKrisz <biro.s.kr@gmail.com>
2022-02-28 14:17:37 +0100

    second JS task is done

--- 1-DOM/dom.js
+++ 1-DOM/dom.js
@@ -1,5 +1,12 @@
 function manipulateDom() {
-    // 
+   const elements = document.querySelectorAll('.quote').forEach( item => {
+       item.style.fontStyle = 'italic'
+       item.style.backgroundColor = 'lightblue'
+       item.style.borderColor = 'blue'
+   })
+
+   return elements
+
 };
 
 export { manipulateDom };
